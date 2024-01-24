876e95d2c1c70c9b49efea9a17c984fa1fba525d
bsaglimb <bsaglimb>
2024-01-24 01:29:18 -0500

    added hbs code, express middleware, and turned on connection to db and server in server.js file

--- server.js
+++ server.js
@@ -25,3 +25,21 @@ const sesh = {
 };
 
 app.use(session(sesh));
+
+const hbs = exphbs.create({ helpers });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+// Express middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+// Turn on connection to db and server
+sequelize.sync({ force: false }).then(() => {
+    app.listen(PORT, () => {
+        console.log('Now listening on port ${PORT}!');
+    });
+});
