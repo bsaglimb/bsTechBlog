d43c7e3222bfd20f4f6c7b14ea2681d178950da5
bsaglimb <bsaglimb>
2024-01-28 18:59:33 -0500

    added files to the models folder and moved files from db that belonged to the controllers file

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes');
-const postRoutes = require('./post-routes');
+const userRoutes = require('./api/user-routes');
+const postRoutes = require('./api/post-routes');
 const commentRoutes = require('./comment-routes');
 
 router.use('/user', userRoutes);
