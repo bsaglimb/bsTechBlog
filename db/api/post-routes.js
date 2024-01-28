69f01e7c9e2145c9b585a27933aa88c270bb34ef
bsaglimb <bsaglimb>
2024-01-28 18:53:14 -0500

    added starter code to the files in the api folder and added code to the index.js in the api folder

---
+++ db/api/post-routes.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+const { User, Post, Comment  } = require('../../models');
+const withAuth = require('../../utils/auth');
+
+
+
+
+module.exports = router;
